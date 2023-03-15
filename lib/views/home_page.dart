4c464e96aa89a6a9cbbf32c36956d5c7049f2de3
Derrick <dmwilliamson@gmail.com>
2023-03-15 17:28:15 -0500

    added curved navigation bar package and implemented in home page

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:rock_hopper/views/launches_page.dart';
 import 'package:rock_hopper/views/news_page.dart';
 import 'package:rock_hopper/views/travel_page.dart';
@@ -9,6 +10,17 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        animationDuration: const Duration(milliseconds: 300),
+        color: Colors.white,
+        backgroundColor: Colors.black,
+        items: const <Widget>[
+          Icon(Icons.newspaper_rounded, size: 30),
+          Icon(Icons.rocket, size: 30),
+          Icon(Icons.calendar_month_rounded, size: 30),
+        ],
+        onTap: (value) {},
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
