dfa3e61720b6f791df20198363aefdffc4edd6d7
Derrick <dmwilliamson@gmail.com>
2023-03-15 16:17:42 -0500

    corrected material app to point to splash_screen in main.dart, made updates to lottie.network animation

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:rock_hopper/views/homepage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,16 +13,24 @@ class _HomePageState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Lottie.network(
-              'https://assets1.lottiefiles.com/packages/lf20_ndj9fzcd.json'),
+              'https://assets7.lottiefiles.com/packages/lf20_qhzucuii.json'),
           ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.black),
+            ),
             onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const SplashScreen()));
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const HomePage()));
             },
-            child: const Text('Get Started'),
+            child: const Text(
+              'Get Started',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ],
       ),
