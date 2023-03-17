a476d4c6d914f59ddf3f7dcaa6a04287ea013122
Derrick <dmwilliamson@gmail.com>
2023-03-17 14:01:01 -0500

    created images folder and added bg image, created background image in weather_page

--- lib/views/main/weather_page.dart
+++ lib/views/main/weather_page.dart
@@ -5,14 +5,45 @@ class WeatherPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Weather Page',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover,
+            alignment: Alignment.center,
+            colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
           ),
+        ),
+        child: Padding(
+          padding:
+              const EdgeInsets.only(top: 50, bottom: 15, left: 15, right: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const <Widget>[
+              Text(
+                "Latest Weather\nat Elysium Planitia",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 28.0),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+            ],
+          ),
+          // body: Center(
+          //   child: Text(
+          //     'Weather Page',
+          //     style: TextStyle(
+          //       fontSize: 30,
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
         ),
       ),
     );
