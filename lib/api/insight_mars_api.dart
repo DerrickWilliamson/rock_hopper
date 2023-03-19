ce75c0fa893b549c25b0c2239ade77c2f95c91bc
Derrick <dmwilliamson@gmail.com>
2023-03-18 21:15:33 -0500

    updated to correct constant uri string for Insight Mars Weather API call

--- lib/api/insight_mars_api.dart
+++ lib/api/insight_mars_api.dart
@@ -2,14 +2,11 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:rock_hopper/models/mars_weather.dart';
+import 'package:rock_hopper/views/constants.dart';
 
 class MarsWeatherRepository {
-  final String _baseUrl = 'https://api.nasa.gov';
-  final String _apiKey = 'your_api_key_here';
-
   Future<MarsWeather> getLatestMarsWeather() async {
-    final url = Uri.parse(
-        '$_baseUrl/insight_weather/?api_key=$_apiKey&feedtype=json&ver=1.0');
+    final url = Uri.parse(kInsightMarsWeatherUrl);
 
     final response = await http.get(url);
 
