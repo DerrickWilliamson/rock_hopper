8dad6a7525ec5952f38a58a07994bdc06d414ac1
Derrick <dmwilliamson@gmail.com>
2023-03-19 08:49:25 -0500

    created open weather api file with future function to call the API

---
+++ lib/api/open_weather_api.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:rock_hopper/models/open_weather.dart';
+import 'package:rock_hopper/views/constants.dart';
+
+class OpenWeatherApi {
+  Future<OpenWeather> getCurrentWeather() async {
+    final url = Uri.parse(kOpenWeatherUrl);
+    final response = await http.get(url);
+
+    if (response.body.isNotEmpty) {
+      final jsonMap = json.decode(response.body);
+      return OpenWeather.fromJson(jsonMap);
+    } else {
+      throw Exception('Failed to load current weather data');
+    }
+  }
+}
