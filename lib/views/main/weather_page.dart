60e25b224b4077d40c384dedb3540c8ea8c5fb18
Derrick <dmwilliamson@gmail.com>
2023-03-22 16:20:10 -0500

    removing insight mars weather api and model class, nasa insight mars rover is not producing weather data at this time

--- lib/views/main/weather_page.dart
+++ lib/views/main/weather_page.dart
@@ -21,12 +21,31 @@ class _WeatherPage2State extends State<WeatherPage> {
 
   Future<void> _loadWeather() async {
     final openWeatherApi = OpenWeatherApi();
+
     final openWeather = await openWeatherApi.getCurrentWeather();
+
     setState(() {
       _openWeather = openWeather;
+
       _isLoading = false;
     });
   }
+
+// idea is to iterate over the list of sol_keys from the JSON object
+// and then use the sol_key to get the corresponding JSON values
+
+// json[json["sol_keys"][i]]["AT"]["av"]
+// string sol = json["sol_keys"][i];
+// var sol_data = json[sol];
+
+// for(int i; i < 50; i++) {
+//   print("Hello World");
+// }
+
+// for (final sol_key in sol_keys) {}
+// }
+
+// for (int i = 0; i < sol_keys.length(); i++) {}
 
   @override
   Widget build(BuildContext context) {
---
+++ notes.dart
@@ -0,0 +1,15 @@
+// idea is to iterate over the list of sol_keys from the JSON object
+// and then use the sol_key to get the corresponding JSON values
+
+// json[json["sol_keys"][i]]["AT"]["av"]
+// string sol = json["sol_keys"][i];
+// var sol_data = json[sol];
+
+// for(int i; i < 50; i++) {
+//   print("Hello World");
+// }
+
+// for (final sol_key in sol_keys) {}
+// }
+
+// for (int i = 0; i < sol_keys.length(); i++) {}
