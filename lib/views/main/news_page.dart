68219a886d16f3d311264f43a73616c14e9af4dc
Derrick <dmwilliamson@gmail.com>
2023-03-24 16:22:04 -0500

    updated news page to display json values

--- lib/views/main/news_page.dart
+++ lib/views/main/news_page.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
 
-class NewsPage extends StatelessWidget {
+import '../../api/news_api.dart';
+import '../../models/news_model.dart';
+
+class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
 
   @override
+  State<NewsPage> createState() => _NewsPageState();
+}
+
+class _NewsPageState extends State<NewsPage> {
+  NewsModel _newsModel = NewsModel('', 0, []);
+  NewsApi _newsApi = NewsApi();
+  bool _isLoading = true;
+
+  Future<void> _getNews() async {
+    final news = await _newsApi.getNews();
+    setState(() {
+      _newsModel = news;
+      _isLoading = false;
+      // print(news.status);
+      // print(news.totalResults);
+      // print(news.articles);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getNews();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'News Page',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
+    return Scaffold(
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Status: ${_newsModel.status}',
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    'Total Results: ${_newsModel.totalResults}',
+                    style: const TextStyle(fontSize: 20.0),
+                  ),
+                  const SizedBox(height: 20.0),
+                  Text(
+                    'Article Title: ${_newsModel.articles[0]['title']}',
+                    style: const TextStyle(fontSize: 20.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
